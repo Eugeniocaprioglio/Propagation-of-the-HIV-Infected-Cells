e584895f80937f3bc7443ae36c77b338e781b9f1
Pupak <pupak.hvalapuno@gmail.com>
2023-01-25 15:00:18 +0100

    System functions and matrices defined

--- Theorem1.py
+++ Theorem1.py
@@ -16,8 +16,8 @@ max_dict = {
     "muv": [7, 10]
     }
 
-# SAVING DICTIONARY TO A FILE  --------------------------------------------------------------------------------
-# ------------------------------------------------------------------------------------------------------------
+# SAVING DICTIONARY TO A FILE  -------------------------------------------------
+# ------------------------------------------------------------------------------
 
 np.save('GlobalVarDictionary.npy', max_dict) # max_dict.npy
 txt_filename = 'GlobalVariablesDictionary.txt' # max_dict.txt (Human eye)
@@ -26,15 +26,14 @@ with  open(txt_filename, 'w') as f :
 
     f.write(str(max_dict))
 
-# BUILDING GENERATOR FUNCTION (used for simulation) ---------------------------------------------
-# ------------------------------------------------------------------------------------------------------------
+# BUILDING GENERATOR FUNCTION (used for simulation) ----------------------------
 
 
 def gen(a,k): # k = max_dict[...][1]
     return abs(math.sin(a**2*k+a*k*3-k**4*a+36))
 
 # ------------------------------------------ SYSTEM STATE FUNCTIONS
-# ------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 def uninfected_steady_state(a):
 
@@ -46,7 +45,7 @@ def uninfected_steady_state(a):
     return [(r - mus + math.sqrt((r - mus)**2 + (4*alpha*r/s_max)))*s_max/(2*r),
     0,
     0]
-# ------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 def infected_steady_state(a):
 
@@ -63,7 +62,7 @@ def infected_steady_state(a):
     alpha/mui - (mus*muv)/(gamma*mui*n) + ((muv*r)/(gamma*mui*n))*(1-(muv/(gamma*n*s_max))),
     alpha*n/muv - mus/gamma + ((r/gamma)*(1-(muv/(gamma*n*s_max)))) ]
 
-# ------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 def reproduction_ratio(a):
 
@@ -74,4 +73,125 @@ def reproduction_ratio(a):
 
     return (gamma*n*s_0)/muv
 
-# ---------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+def n_parameters(a):
+
+    alpha = max_dict["alpha"][1]*gen(a,max_dict["alpha"][0])
+    mus = max_dict["mus"][1]*gen(a,max_dict["mus"][0])
+    s_max = max_dict["s_max"][1]*gen(a,max_dict["s_max"][0])
+    gamma = max_dict["gamma"][1]*gen(a,max_dict["gamma"][0])
+    mui = max_dict["mui"][1]*gen(a,max_dict["mui"][0])
+    muv = max_dict["muv"][1]*gen(a,max_dict["muv"][0])
+
+    return [(gamma**2)*mui*s_max - 4*alpha*(mui + muv),
+            - 2*gamma*mui*muv*((mui**2 + 3*mui*muv + muv**2)*s_max - 4*alpha*(mui + muv)),
+            muv*(mui**4 + 2*(mui**2)*muv*(3*mui - 2*mus) + 6*mui*(muv**3) + mui*(muv**2)*(11*mui - 4*mus) + muv**4)
+           ]
+# ------------------------------------------------------------------------------
+def r_parameters(a):
+
+    alpha = max_dict["alpha"][1]*gen(a,max_dict["alpha"][0])
+    mus = max_dict["mus"][1]*gen(a,max_dict["mus"][0])
+    s_max = max_dict["s_max"][1]*gen(a,max_dict["s_max"][0])
+    gamma = max_dict["gamma"][1]*gen(a,max_dict["gamma"][0])
+    mui = max_dict["mui"][1]*gen(a,max_dict["mui"][0])
+    muv = max_dict["muv"][1]*gen(a,max_dict["muv"][0])
+    n = max_dict["n"][1]*gen(a,max_dict["n"][0])
+
+    return [(muv**4)*(mui + muv),
+    gamma*(muv**2)*n*s_max*(-gamma*mui*muv*n*s_max + 2*alpha*gamma*mui*n + 2*alpha*gamma*muv*n + (mui**2)*muv + muv**3),
+    (n**2)*(gamma**2)*(s_max**2)*(alpha*gamma*(muv**3)*n + alpha*gamma*mui*muv*n*(mui + muv) + (alpha**2)*(gamma**2)*(n**2)*(mui + muv) + mui*mus*muv**3),
+    ]
+
+# ------------------------------------------------------------------------------
+
+    def state_is_in_P(a):
+
+    n = max_dict["n"][1]*gen(a,max_dict["n"][0])
+    r = max_dict["r"][1]*gen(a,max_dict["r"][0])
+    [an, bn, cn] = n_parameters(a)
+    [ar, br, cr] = r_parameters(a)
+    root_1 = (-bn + (bn**2 -4*an*cn)**0.5)/(2*an)
+    root_2 = (-bn - (bn**2 -4*an*cn)**0.5)/(2*an)
+
+    max_root = max(root_1, root_2)
+
+    a = (n > max_root) # boolean: true/false
+
+    b = (ar*r**2+br*r+cr < 0)
+
+    if (a == True and b==True):
+        return True
+
+    return False
+
+def choose_in_p():
+
+    arr = []
+    for a in range(12):
+        if state_is_in_P(a):
+            arr.append(a)
+
+    return arr
+
+#print(choose_in_p()) # print "a" parameters for which (N,r) belongs to set P
+
+state_is_in_p_array = choose_in_p()
+
+# SYSTEM STATE MATRICES --------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+# BUILDING MATRIX FOR STEADY STATE S_0, I_0=0, V_0=0 ---------------------------
+
+row = len(choosen_a_array)
+col = 3
+
+steady_matrix = np.zeros((row, col))
+
+for i in range(0, row):
+    steady_matrix[i][0] = uninfected_steady_state(choosen_a_array[i])[0]
+
+# BUILDING MATRIX FOR STEADY STATE S_1, I_1, V_1 only for r > 1 (state is in P)
+
+rowP = len(state_is_in_p_array)
+colP = 3
+
+steady_P_matrix = np.zeros((rowP, colP))
+
+for i in range (0, len(state_is_in_p_array)):
+    steady_P_matrix [i, :] = infected_steady_state(state_is_in_p_array[i])
+
+# Saving values for which state is (not) in P into a file ------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------
+
+arr = np.asarray(choosen_a())
+np.save("state_is_not_in_P.npy", arr) # binary data
+np.savetxt('state_is_not_in_P.txt', arr) # human readable data
+
+arr2 = np.asarray(choose_in_p())
+np.save("state_is_in_P.npy", arr2) # binary data
+np.savetxt('state_is_in_P.txt', arr2) # human readable data
+
+a_used = np.concatenate((arr, arr2))
+
+print (a_used)
+
+
+# Trying to save matrix S_0/I_0/V_0 to file ------------------------------------
+# ------------------------------------------------------------------------------
+
+#Binary data
+np.save('S_0_matrix.npy', steady_matrix)
+
+#Human readable data
+np.savetxt('S_0_matrix.txt', steady_matrix)
+
+# Trying to save matrix S_1/I_1/V_1 to binary file -----------------------------
+# ------------------------------------------------------------------------------
+
+#Binary data
+np.save('S_P_matrix.npy', steady_P_matrix)
+
+#Human readable data
+np.savetxt('S_P_matrix.txt', steady_P_matrix)
